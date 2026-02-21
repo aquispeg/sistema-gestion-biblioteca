d2f781592154fa6f41aeab9049a6b69ddde6ffb8
aquispeg <aquispeg@unjbg.edu.pe>
2026-02-21 15:23:06 -0500

    modificacion de la clase recurso biblioteca, haciendo uso de polimorfismo

--- recursoBiblioteca.ts
+++ recursoBiblioteca.ts
@@ -1,12 +1,35 @@
+import {CategoriaLibro} from "./index";
+
 export abstract class RecursoBiblioteca{
-    id:string;
+    protected readonly id:string;
     titulo:string;
-    categoria: categoriaLibro;
-    fechaRegistro: Date;
-    constructor(id:string,titulo:string, categoria: categoriaLibro){
+    categoria: CategoriaLibro;
+    protected readonly fechaRegistro: Date;
+    constructor(id:string,titulo:string, categoria: CategoriaLibro){
         this.id=id;
         this.titulo=titulo;
         this.categoria=categoria;
         this.fechaRegistro= new Date();
     }
+    abstract esPrestable():boolean;
+
+    getId(): string {
+    return this.id;
+  }
+
+  getTitulo(): string {
+    return this.titulo;
+  }
+
+  getCategoria(): CategoriaLibro {
+    return this.categoria;
+  }
+
+  getFechaRegistro(): Date {
+    return this.fechaRegistro;
+  }
+
+  toString(): string {
+    return `[${this.id}] ${this.titulo} - ${this.categoria.toString()}`;
+  }
 }
