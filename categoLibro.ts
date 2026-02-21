310e3ffe601f1a4ff61345f1e180bf3c2c0c38d4
aquispeg <aquispeg@unjbg.edu.pe>
2026-02-20 20:54:57 -0500

    agregacion del tsconfig.json, necesario para la compilacion

--- categoLibro.ts
+++ categoLibro.ts
@@ -1,6 +1,22 @@
+import {CategoriaLibro} from "./index";
 export class categoriaLibro{
-    constructor(
-        public nombre:string,
-        public descripcion:string
-    ){}
+    private static _contador =1;
+
+    readonly id: string;
+    nombre:string;
+    CategoriaLibro: categoriaLibro;
+    diasMaxPrestamo: number;
+
+    constructor(nombre:string,CategoriaLibro: categoriaLibro,diasMaxPrestamo:number=3){
+        this.id = `CAT- ${categoriaLibro._contador++}`;
+        this.nombre=nombre;
+        this.CategoriaLibro=CategoriaLibro;
+        this.diasMaxPrestamo=diasMaxPrestamo;
+    }
+    esPrestable():boolean{
+        return true;
+    }
+    toString():string{
+        return `[${this.id}] ${this.nombre} (${this.CategoriaLibro}) - Máx. ${this.diasMaxPrestamo} días`;
+    }
 }
